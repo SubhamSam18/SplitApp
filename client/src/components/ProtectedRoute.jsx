fd6a69318b9279a0b631143d988908314dd3bffb
SubhamSam18 <subhamsamanta189@gmail.com>
2026-02-16 23:02:29 +0530

    Updated to cookie storage in ProtectedRoute

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,33 @@
 import { Navigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+function ProtectedRoute({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-function ProtectedRoute({childern}){
-    const token = localStorage.getItem("token");
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await axios.get("http://localhost:5000/api/auth/me", {
+          withCredentials: true,
+        });
+        setIsAuthenticated(true);
+      } catch (err) {
+        setIsAuthenticated(false);
+      }
+    };
 
-    if(!token){
-        return <Navigate to="/login"/>
-    }
+    checkAuth();
+  }, []);
 
-    return children;
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/auth" />;
+  }
+
+  return children;
 }
 
 export default ProtectedRoute;

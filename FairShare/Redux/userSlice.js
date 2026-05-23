3944fc0ce98b9920e4fefb6bcec1052fa2191baf
SubhamSam18 <subhamsamanta189@gmail.com>
2026-05-23 22:23:07 +0530

    Fixed user data persistent issue in redux

--- FairShare/Redux/userSlice.js
+++ FairShare/Redux/userSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     userData: null,
-    isAuthenticated: true,
+    isAuthenticated: false,
 };
 
 const userSlice = createSlice({
--- FairShare/src/navigator/navigation.tsx
+++ FairShare/src/navigator/navigation.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { AuthStack } from './AuthStack';
 import { MainStack } from './MainStack';
+import type { RootState } from '../../Redux/store';
 
 export const RootNavigator = () => {
-    const isAuthenticated = useSelector((state:any)=>state.user.isAuthenticated)
-
-    if (isAuthenticated) {
-        return <MainStack />;
-    }
-
-    return <AuthStack />;
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated);
+  return isAuthenticated ? <MainStack /> : <AuthStack />;
 };
